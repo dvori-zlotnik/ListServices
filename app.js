79f5c684ad709fd28c96ecafdf1f76e95de3b372
dvori <dvori7330@gmail.com>
2025-04-23 17:35:30 +0300

    add env

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import renderApi from '@api/render-api';
-
+import dotenv from 'dotenv'
+dotenv.config(); // טוען את משתני הסביבה מהקובץ .env
 const app = express()
-const port = 3001
+const port = process.env.PORT
 
 app.use(bodyParser.json())
 app.get('',(req,res)=>{
