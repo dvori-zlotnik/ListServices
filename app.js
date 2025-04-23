0355a52231c6213732b35163403b69b1f5062250
dvori <dvori7330@gmail.com>
2025-04-23 17:19:01 +0300

    change api key

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import renderApi from '@api/render-api';
+
+const app = express()
+const port = 3001
+
+app.use(bodyParser.json())
+app.get('',(req,res)=>{
+    renderApi.auth(process.env.API_KEY);
+    renderApi.listServices({includePreviews: 'true', limit: '20'})
+      .then(({ data }) => res.status(200).send(data))
+      .catch(err => console.error(err));
+})
+app.listen(port, () => {
+    console.log(`my application is running on http://localhost:${port}`)
+})
